6c3bb172d94972dcf281b4bb27b66da8e645b3c5
Tobias Boesch <tboesch@gmail.com>
2020-01-08 19:37:43 +0100

    Add partial implementation for Eltako's A5-12-01 variant

--- src/main/java/org/openhab/binding/enocean/internal/eep/A5_12/A5_12_01_Eltako.java
+++ src/main/java/org/openhab/binding/enocean/internal/eep/A5_12/A5_12_01_Eltako.java
@@ -12,10 +12,21 @@
  */
 package org.openhab.binding.enocean.internal.eep.A5_12;
 
+import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.core.library.types.QuantityType;
 import org.eclipse.smarthome.core.library.unit.SmartHomeUnits;
 import org.eclipse.smarthome.core.types.State;
+import org.eclipse.smarthome.core.types.UnDefType;
+import org.eclipse.smarthome.core.util.HexUtils;
+import org.openhab.binding.enocean.internal.eep.EEPHelper;
 import org.openhab.binding.enocean.internal.messages.ERP1Message;
+
+import java.util.function.Function;
+
+import static org.openhab.binding.enocean.internal.EnOceanBindingConstants.CHANNEL_INSTANTPOWER;
+import static org.openhab.binding.enocean.internal.EnOceanBindingConstants.CHANNEL_TOTALUSAGE;
+import static org.openhab.binding.enocean.internal.EnOceanBindingConstants.CHANNEL_INSTANTPOWER_ALTERNATETARIFF;
+import static org.openhab.binding.enocean.internal.EnOceanBindingConstants.CHANNEL_TOTALUSAGE_ALTERNATETARIFF;
 
 /**
  *
@@ -27,13 +38,45 @@ public class A5_12_01_Eltako extends A5_12 {
         super(packet);
     }
 
-    @Override
-    protected State calcCumulativeValue(float value) {
-        return new QuantityType<>(value, SmartHomeUnits.KILOWATT_HOUR);
+    protected State getPowerValue(boolean secondaryTariff) {
+        byte db0 = getDB_0();
+
+        if (((db0 == 0x1C) && secondaryTariff) || ((db0 == 0x0C) && !secondaryTariff)) {
+            float currentValue = Long.parseLong(HexUtils.bytesToHex(new byte[] { getDB_3(), getDB_2(), getDB_1() }), 16);
+            return new QuantityType<>(currentValue, SmartHomeUnits.WATT);
+        }
+
+        return UnDefType.UNDEF;
+    }
+
+    protected State getWorkValue(boolean secondaryTariff) {
+        byte db0 = getDB_0();
+
+        if (((db0 == 0x19) && secondaryTariff) || ((db0 == 0x09) && !secondaryTariff)) {
+            float currentValue = Long.parseLong(HexUtils.bytesToHex(new byte[] { getDB_3(), getDB_2(), getDB_1() }), 16)
+                            * (float) 0.1;
+            return new QuantityType<>(currentValue, SmartHomeUnits.KILOWATT_HOUR);
+        }
+
+        return UnDefType.UNDEF;
     }
 
     @Override
-    protected State calcCurrentValue(float value) {
-        return new QuantityType<>(value, SmartHomeUnits.WATT);
+    protected State convertToStateImpl(String channelId, String channelTypeId, Function<String, State> getCurrentStateFunc,
+                                       Configuration config) {
+        switch (channelId) {
+            case CHANNEL_INSTANTPOWER:
+                return getPowerValue(false);
+            case CHANNEL_TOTALUSAGE:
+                State value = getWorkValue(false);
+                State currentState = getCurrentStateFunc.apply(channelId);
+                return EEPHelper.validateTotalUsage(value, currentState, config);
+            case CHANNEL_INSTANTPOWER_ALTERNATETARIFF:
+                return getPowerValue(true);
+            case CHANNEL_TOTALUSAGE_ALTERNATETARIFF:
+                return getWorkValue(true);
+        }
+
+        return UnDefType.UNDEF;
     }
 }
